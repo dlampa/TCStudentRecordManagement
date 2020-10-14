0d46b19a78951d400f2e95e29e9e6fc2403b02c1
Damir Lampa <lampa@altx.dev>
2020-10-14 17:06:47 -0600

    Completed Add target for TaskTypesController;

--- TCStudentRecordManagement/Controllers/BLL/TaskTypeBLL.cs
+++ TCStudentRecordManagement/Controllers/BLL/TaskTypeBLL.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using TCStudentRecordManagement.Controllers.Exceptions;
 using TCStudentRecordManagement.Models;
+using TCStudentRecordManagement.Models.DTO;
 
 namespace TCStudentRecordManagement.Controllers.BLL
 {
-    public class TaskTypeBLL
+    internal class TaskTypeBLL
     {
         private readonly DataContext _context;
 
@@ -14,5 +17,44 @@ namespace TCStudentRecordManagement.Controllers.BLL
         {
             _context = context;
         }
+
+        /// <summary>
+        /// Business logic for processing TaskType record addition
+        /// </summary>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        internal object AddTaskTypeBLL(string description)
+        {
+            // Create a new APIException object to store possible exceptions as checks are performed. 
+            APIException exceptionList = new APIException();
+
+            Dictionary<string, bool> exceptionTests = new Dictionary<string, bool>()
+            {
+                { "description parameter cannot be empty", description == null || description.Trim() == string.Empty },
+                { "description must be unique", _context.TaskTypes.Any(x => x.Description.ToLower() == description.Trim().ToLower()) }
+            };
+
+            foreach (KeyValuePair<string, bool> kvp in exceptionTests)
+            {
+                if (kvp.Value) exceptionList.AddExMessage(kvp.Key);
+            }
+
+            if (!exceptionList.HasExceptions)
+            {
+                // Capitalise the first letter of the first word of the description
+                string dbDescription = $"{description.Trim().Substring(0, 1).ToUpper()}{description.Trim().Substring(1)?.ToLower()}";
+
+                TaskTypeDTO newDbObject = new TaskTypeDTO { Description = dbDescription };
+
+                return newDbObject;
+            }
+            else
+            {
+                return exceptionList;
+            }
+
+        } // End of AddTaskTypeBLL
+
+
     }
 }
--- TCStudentRecordManagement/Controllers/TaskTypesController.cs
+++ TCStudentRecordManagement/Controllers/TaskTypesController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TCStudentRecordManagement.Controllers.BLL;
+using TCStudentRecordManagement.Controllers.Exceptions;
 using TCStudentRecordManagement.Models;
 using TCStudentRecordManagement.Models.DTO;
 using TCStudentRecordManagement.Utils;
@@ -24,6 +26,7 @@ namespace TCStudentRecordManagement.Controllers
         {
             _context = context;
         }
+
         /// <summary>
         /// Get a list of all the records from the TaskTypes table
         /// </summary>
@@ -42,80 +45,152 @@ namespace TCStudentRecordManagement.Controllers
             return result;
 
         }
-
-        // GET: api/TaskTypes/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<TaskType>> GetTaskType(int id)
+        
+        /// <summary>
+        /// Add a TaskType record to the TaskTypes table
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        [HttpPut("add")]
+        [Authorize(Policy = "StaffMember")]
+        public async Task<ActionResult> AddTaskType(string description)
         {
-            var taskType = await _context.TaskTypes.FindAsync(id);
 
-            if (taskType == null)
+            // Call BLL TaskType Add method with all the parameters
+            object BLLResponse = new TaskTypeBLL(_context).AddTaskTypeBLL(description: description);
+
+            // Get the base class for the response
+            // Ref: https://docs.microsoft.com/en-us/dotnet/api/system.type.basetype?view=netcore-3.1
+            if (BLLResponse.GetType().BaseType == typeof(Exception))
             {
-                return NotFound();
+                // Create log entries for Debug log
+                ((APIException)BLLResponse).Exceptions.ForEach(ex => Logger.Msg<TaskTypesController>((Exception)ex, Serilog.Events.LogEventLevel.Debug));
+
+                // Return response from API
+                return BadRequest(new { errors = ((APIException)BLLResponse).Exceptions.Select(x => x.Message).ToArray() });
             }
-
-            return taskType;
-        }
-
-        // PUT: api/TaskTypes/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTaskType(int id, TaskType taskType)
-        {
-            if (id != taskType.TypeID)
+            else
             {
-                return BadRequest();
-            }
-
-            _context.Entry(taskType).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TaskTypeExists(id))
+                try
                 {
-                    return NotFound();
+                    TaskType newTaskType = new TaskType { Description = ((TaskTypeDTO)BLLResponse).Description };
+
+                    // Create the record
+                    _context.TaskTypes.Add(newTaskType);
+                    await _context.SaveChangesAsync();
+
+                    Logger.Msg<CohortsController>($"[{User.FindFirstValue("email")}] [ADD] TaskType '{description}' successful", Serilog.Events.LogEventLevel.Information);
+
+                    // Convert back to DTO and return to user
+                    TaskTypeDTO response = new TaskTypeDTO(newTaskType);
+                    return Ok(response);
+                }
+                catch (Exception ex)
+                {
+                    // Local log entry. Database reconciliation issues are more serious so reported as Error
+                    Logger.Msg<TaskTypesController>($"[ADD] Database sync error {ex.Message}", Serilog.Events.LogEventLevel.Error);
+
+                    // Return response to client
+                    return StatusCode(500, new { errors = "Database update failed. Contact the administrator to resolve this issue." });
+                }
+            }
+
+        }
+/*
+        // PUT: Modify Cohort [CohortBLL] [Return DTO]
+        [HttpPut("modify")]
+        [Authorize(Policy = "StaffMember")]
+        public async Task<ActionResult> ModifyCohort_Target([FromBody] CohortDTO cohort)
+        {
+
+            if (CohortExists(cohort.CohortID))
+            {
+
+                // Call BLL Cohort Add method with all the parameters
+                object BLLResponse = new CohortBLL(_context).ModifyCohort(cohort: cohort);
+
+                // Get the base class for the response
+                // Ref: https://docs.microsoft.com/en-us/dotnet/api/system.type.basetype?view=netcore-3.1
+                if (BLLResponse.GetType().BaseType == typeof(Exception))
+                {
+                    // Create log entries for Debug log
+                    ((APIException)BLLResponse).Exceptions.ForEach(ex => Logger.Msg<CohortsController>((Exception)ex, Serilog.Events.LogEventLevel.Debug));
+
+                    // Return response from API
+                    return BadRequest(new { errors = ((APIException)BLLResponse).Exceptions.Select(x => x.Message).ToArray() });
                 }
                 else
                 {
-                    throw;
+                    try
+                    {
+                        // Find the existing record based on ID
+                        Cohort currentRecord = _context.Cohorts.Where(x => x.CohortID == cohort.CohortID).First();
+
+                        // Modify the record
+                        currentRecord.Name = ((Cohort)BLLResponse).Name;
+                        currentRecord.StartDate = ((Cohort)BLLResponse).StartDate;
+                        currentRecord.EndDate = ((Cohort)BLLResponse).EndDate;
+
+                        // Save changes
+                        await _context.SaveChangesAsync();
+
+                        Logger.Msg<CohortsController>($"[{User.Claims.Where(x => x.Type == "email").FirstOrDefault().Value}] [MODIFY] CohortID: {cohort.CohortID} successful", Serilog.Events.LogEventLevel.Information);
+
+                        // Return modified record as a DTO
+                        CohortDTO response = new CohortDTO(currentRecord);
+                        return Ok(response);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Local log entry. Database reconciliation issues are more serious so reported as Error
+                        Logger.Msg<CohortsController>($"[MODIFY] Database sync error {ex.Message}", Serilog.Events.LogEventLevel.Error);
+
+                        // Return response to client
+                        return StatusCode(500, new { errors = "Database update failed. Contact the administrator to resolve this issue." });
+                    }
                 }
             }
-
-            return NoContent();
-        }
-
-        // POST: api/TaskTypes
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<TaskType>> PostTaskType(TaskType taskType)
-        {
-            _context.TaskTypes.Add(taskType);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTaskType", new { id = taskType.TypeID }, taskType);
-        }
-
-        // DELETE: api/TaskTypes/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<TaskType>> DeleteTaskType(int id)
-        {
-            var taskType = await _context.TaskTypes.FindAsync(id);
-            if (taskType == null)
+            else
             {
                 return NotFound();
             }
+        } // End of ModifyCohort_Target
 
-            _context.TaskTypes.Remove(taskType);
-            await _context.SaveChangesAsync();
+        // DELETE: Delete Cohort [NO BLL] [Return STATUS]
+        [HttpDelete("delete")]
+        [Authorize(Policy = "SuperAdmin")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            // Find existing Cohort record in DB
+            Cohort cohort = await _context.Cohorts.FindAsync(id);
 
-            return taskType;
+            if (cohort == null)
+            {
+                Logger.Msg<CohortsController>($"[{User.Claims.Where(x => x.Type == "email").FirstOrDefault().Value}] [DELETE] CohortID: {id} not found", Serilog.Events.LogEventLevel.Debug);
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Cohorts.Remove(cohort);
+                await _context.SaveChangesAsync();
+
+                Logger.Msg<CohortsController>($"[{User.Claims.Where(x => x.Type == "email").FirstOrDefault().Value}] [DELETE] CohortID: {id} success", Serilog.Events.LogEventLevel.Information);
+                return Ok(new CohortDTO(cohort));
+            }
+            catch (Exception ex)
+            {
+                // Probably due to FK violation
+                Logger.Msg<CohortsController>($"[DELETE] Database sync error {ex.Message}", Serilog.Events.LogEventLevel.Error);
+
+                // Return response to client
+                return StatusCode(500, new { errors = "Database update failed. Perhaps there are students in this cohort?" });
+            }
+
         }
+*/
 
         private bool TaskTypeExists(int id)
         {
