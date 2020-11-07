bf08d1a496ec2132ff489b6679173e5c9b7ed416
Damir Lampa <lampa@altx.dev>
2020-11-06 23:25:00 -0700

    Added StaffBLL;

---
+++ TCStudentRecordManagement/Controllers/BLL/StaffBLL.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TCStudentRecordManagement.Controllers.Exceptions;
+using TCStudentRecordManagement.Models;
+using TCStudentRecordManagement.Models.DTO;
+
+namespace TCStudentRecordManagement.Controllers.BLL
+{
+    internal class StaffBLL
+    {
+        private readonly DataContext _context;
+
+        internal StaffBLL(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Business logic for creating staff records in database
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="superUser"</param>
+        /// <returns>BLL result object</returns>
+        internal object AddStaffBLL(int userID, bool superUser)
+        {
+            // Create a new APIException object to store possible exceptions as checks are performed. 
+            APIException exceptionList = new APIException();
+
+            Dictionary<string, bool> exceptionTests = new Dictionary<string, bool>()
+            {
+                { "Specified userID does not exist", !_context.Users.Any(x => x.UserID == userID) }
+            };
+
+            foreach (KeyValuePair<string, bool> kvp in exceptionTests)
+            {
+                if (kvp.Value) exceptionList.AddExMessage(kvp.Key);
+            }
+
+            if (!exceptionList.HasExceptions)
+            {
+                StaffDTO newStaff = new StaffDTO
+                {
+                    UserID = userID,
+                    SuperUser = superUser
+                };
+
+                return newStaff;
+            }
+            else
+            {
+                return exceptionList;
+            }
+
+        } // End of AddStaffBLL
+
+        /// <summary>
+        /// Business logic for modifying Staff records
+        /// </summary>
+        /// <param name="staff"></param>
+        /// <returns></returns>
+        internal object ModifyUserBLL(StaffDTO staff)
+        {
+            APIException exceptionList = new APIException();
+
+            Dictionary<string, bool> exceptionTests = new Dictionary<string, bool>()
+            {
+                { "Specified UserID does not exist", !_context.Users.Any(x => x.UserID == staff.UserID) },
+                { "Specified StaffID does not exist", !_context.Staff.Any(x => x.StaffID == staff.StaffID) }
+            };
+
+            foreach (KeyValuePair<string, bool> kvp in exceptionTests)
+            {
+                if (kvp.Value) exceptionList.AddExMessage(kvp.Key);
+            }
+
+            if (!exceptionList.HasExceptions)
+            {
+                return staff;
+            }
+            else
+            {
+                return exceptionList;
+            }
+
+        } // End of ModifyStaff
+
+    } // End of StaffBLL
+}
--- TCStudentRecordManagement/Controllers/StaffController.cs
+++ TCStudentRecordManagement/Controllers/StaffController.cs
@@ -76,65 +76,7 @@ namespace TCStudentRecordManagement.Controllers
 
         } // End of Get
 
-        // PUT: api/Staff/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutStaff(int id, Staff staff)
-        {
-            if (id != staff.StaffID)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(staff).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StaffExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Staff
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Staff>> PostStaff(Staff staff)
-        {
-            _context.Staff.Add(staff);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetStaff", new { id = staff.StaffID }, staff);
-        }
-
-        // DELETE: api/Staff/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Staff>> DeleteStaff(int id)
-        {
-            var staff = await _context.Staff.FindAsync(id);
-            if (staff == null)
-            {
-                return NotFound();
-            }
-
-            _context.Staff.Remove(staff);
-            await _context.SaveChangesAsync();
-
-            return staff;
-        }
+       
 
         private bool StaffExists(int id)
         {
